2a7b3608b9695b277a57eb4864fc15ad51d62e56
justin-r-oliver <justin.r.oliver@gmail.com>
2015-07-15 20:53:56 +0100

    Initial commit

---
+++ StacksAndQueues-Nesting/src/Solution.java
@@ -0,0 +1,76 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can use System.out.println for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+
+    int head, tail =0;
+
+    int N;
+
+    String parens[] = new String[1000000];
+
+    // Correctness 66%, Performance 80%, Overall 75%
+    public int solution(String S) {
+        // write your code in Java SE 8
+
+        N = S.length();
+
+        for (int i=0; i < N; i++) {
+            String val = S.substring(i, i+1);
+
+            if (val.equals("(")) {
+                push(val);
+            } else {
+                pop();
+            }
+
+            // WARNING: producing output may seriously slow down your code!
+//            System.out.println(String.format("%s %d %d", val, head, tail));
+
+            if (head > tail) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+    // correctness 66% performance 0%, overall 25%
+    public int solution1(String S) {
+        // write your code in Java SE 8
+
+        N = S.length();
+
+        for (int i=0; i < N; i++) {
+            String val = S.substring(i, i+1);
+
+            if (val.equals("(")) {
+                push(val);
+            } else {
+                pop();
+            }
+
+            // WARNING: producing output may seriously slow down your code!
+            System.out.println(String.format("%s %d %d", val, head, tail));
+
+            if (head > tail) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+
+    String pop() {
+        head = (head + 1) % N;
+        return parens[head];
+    }
+
+    void push(String val) {
+
+        tail = (tail + 1) % N;
+        parens[tail] = val;
+    }
+}
---
+++ StacksAndQueues-Nesting/src/SolutionTest.java
@@ -0,0 +1,38 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by justin on 15/07/15.
+ */
+public class SolutionTest {
+
+    @Test
+    public void test1() {
+
+        String S = "(()(())())";
+
+
+        Assert.assertEquals(1, new Solution().solution(S));
+
+
+
+    }
+
+    @Test
+    public void test2() {
+
+        String S = "())";
+
+
+        Assert.assertEquals(0, new Solution().solution(S));
+        Assert.assertEquals(0, new Solution().solution("("));
+        Assert.assertEquals(0, new Solution().solution(")"));
+        Assert.assertEquals(0, new Solution().solution("(()"));
+
+
+
+    }
+
+}
